d4766e0a8c9dc1cf0e4937bc8e4854672bb81e19
Jerry Baskoro <bjbbeat@gmail.com>
2024-09-19 02:12:10 +0700

    delete unused

--- src/app/components/TestSurvey.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const TestSurvey = () => {
-  return (
-    <div>TestSurvey</div>
-  )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 
 
+
 export default function Home() {
   return (
-   <h1>hellow</h1>
+    <div>
+
+    <h1>hellow</h1>
+    </div>
+    
   );
 }
